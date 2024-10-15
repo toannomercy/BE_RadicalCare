64a5eada09cef0b2a99cec8e7a850b085642de16
toannomercy <htoan2k1@gmail.com>
2024-10-15 22:56:34 +0700

    fix bug

--- src/main/java/com/radical/be_radicalcare/Repositories/IProductRepository.java
+++ src/main/java/com/radical/be_radicalcare/Repositories/IProductRepository.java
@@ -2,6 +2,8 @@ package com.radical.be_radicalcare.Repositories;
 
 import com.radical.be_radicalcare.Entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IProductRepository extends JpaRepository<Product, Long> {
 }
